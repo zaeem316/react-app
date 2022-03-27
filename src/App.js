35ef5e04edf38b679b20ec75889b42f38503ff32
Zaeem Ahmad <zaeem@whydonate.nl>
2022-03-28 01:43:26 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 
@@ -6,15 +7,29 @@ function App() {
   const [keyword, setKeyword] = useState('')
   const [images, setImages] = useState([]);
 
-  async function getImages() {
-    const data = await fetch('https://serverless-api.zaeem316.workers.dev', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query: `${keyword}` }),
-    })
-    setImages(...data)
+  function getImages() {
+
+    axios.post(`https://serverless-api.zaeem316.workers.dev`, { query: `car` }, {mode: 'no-cors'})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+
+    // fetch('https://serverless-api.zaeem316.workers.dev', {
+    //   method: 'POST', // or 'PUT'
+    //   mode: 'no-cors',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({ query: `car` }),
+    // })
+    // .then(data => data[body].json())
+    // .then(a => console.log(a))
+    // const imagesArray = await resp.json();
+    // const imagesArray = resp.body;
+    // console.log(imagesArray)
+    // setImages(data)
   }
 
   return (
@@ -22,7 +37,7 @@ function App() {
       <div className='d-flex justify-content-center pt-4'>
         <div className='w-50'>
           <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search Images" onChange={(e)=> setKeyword(e.target.value)} />
+            <input type="text" className="form-control" placeholder="Search Images" onChange={(e) => setKeyword(e.target.value)} />
             <div className="input-group-append">
               <button className="btn btn-outline-success" type="button" onClick={() => { getImages() }}>
                 Button
